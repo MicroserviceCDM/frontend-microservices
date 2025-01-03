c2cab58a2d2260fdea2ac9e18efe3598e6c80200
tuilakhanh <bacdau852@gmail.com>
2025-01-03 22:37:45 +0000

    Fix chat

--- src/pages/Manager/ManagerChat/ManagerChat.jsx
+++ src/pages/Manager/ManagerChat/ManagerChat.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ChatRoom from '../../../components/Chat/ChatRoom'
 import ManagerSideBar from '../../../layouts/components/ManagerSideBar'
 
-const ManagerChat = () => {
-  
-
+function ManagerChat() {
   return (
-    <div className="bg-gray-100 dark:bg-gray-900 h-5/6">
+    <div className="bg-gray-100 dark:bg-gray-900 min-h-screen">
       <div className="flex">
         <ManagerSideBar />
-        <div className="flex-auto h-5/6">
+        <div className="flex-1 min-h-min"> {/* Main content area with padding */}
           <ChatRoom />
         </div>
       </div>
     </div>
   )
 }
+
+export default ManagerChat;
--- src/pages/Staff/StaffHome/StaffHome.jsx
+++ src/pages/Staff/StaffHome/StaffHome.jsx
@@ -4,68 +4,6 @@ import React, { useState } from "react";
 import ChatRoom from "../../../components/Chat/ChatRoom";
 
 function StaffHome() {
-  // Simulated data
-  const users = [
-    {
-      id: 1,
-      name: "John Smith",
-      address: "12/255 Nguyen Huu Canh, district 9, HCMC",
-      phone: "(+84)94123456",
-      email: "johnS@gmail.com",
-      notes: "This guy will pay this car by weekly instalment.",
-      chat: [
-        "Hi. i offer an advice for choosing car. Can you help me?",
-        "Ok sure. Which model do you like?",
-        "Well. I'm looking for ModelX at first because it looks so cool. However, its price's extremely high so that I can't afford that. Is there any other cars have same appearance but it's much cheaper?",
-      ],
-    },
-    {
-      id: 2,
-      name: "CR7",
-      address: "24/11 No Trang Long, Binh Thanh district, HCMC",
-      phone: "(+84)94777776",
-      email: "crBuoi@gmail.com",
-      notes: "This guy is rich.",
-      chat: [
-        "Hi. i need to buy 10 cars. Can you help me?",
-        "Ok Bro. Which model do you like?",
-        "Well. I'm looking for ModelS. How much for 10 cars?",
-      ],
-    },
-    {
-      id: 3,
-      name: "Elon Musk",
-      address: "24 Wall.st , Financial district, NYC",
-      phone: "(+84)99999999",
-      email: "richestguy@gmail.com",
-      notes: "This guy is the richest.",
-      chat: [
-        "Hi. i need to buy your web. Can you help me?",
-        "Ok Bro. 1000000000 dollar",
-        "OK, deal!",
-      ],
-    },
-    // ... other users
-  ];
-
-  const [selectedUser, setSelectedUser] = useState(users[0]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleUserClick = (user) => {
-    setSelectedUser(user);
-  };
-
-  const filteredUsers = searchTerm
-    ? users.filter((user) =>
-        user.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    : users;
-
-  function capitalizeFirstLetter(string) {
-    const capitalized = string.charAt(0).toUpperCase();
-    return <div>{capitalized}</div>;
-  }
-
   return (
     <div className="bg-gray-100 dark:bg-gray-900 min-h-screen">
       <div className="flex">
